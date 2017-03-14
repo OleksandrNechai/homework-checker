65a4129b48d8b5c7100ff3f91d14cbcd9b01bef7
OleksandrNechai <nechais@gmail.com>
2017-03-15 00:10:53 +0200

    Fix identations

--- src/App.js
+++ src/App.js
@@ -3,42 +3,46 @@ import React, { Component } from 'react';
 
 
 class Button extends React.Component {
-  render() {
-    return (<div className="fb-login-button" data-max-rows="1" data-size="xlarge" data-show-faces="false" data-auto-logout-link="true"></div>);
-  }
+    render() {
+        return (<div className="fb-login-button" data-max-rows="1" data-size="xlarge" data-show-faces="false" data-auto-logout-link="true"></div>);
+    }
 }
 
 class App extends Component {
-  render() {
-    this.props.fb.getLoginStatus((response) => {
-      if (response.status === 'connected') {
-        const accessToken = response.authResponse.accessToken;
-        if (response.authResponse) {
-          console.log('Welcome!  Fetching your information.... ');
-          this.props.fb.api('/me', { fields: 'name,email,id' }, function (response) {
-            console.log('Good to see you, ' + response.name + '.');
-            fetch('/api/login', {
-              method: 'POST',
-              body: JSON.stringify({ ...response, accessToken }),
-              headers: {
-                accept: 'application/json',
-                "content-type": "application/json"
-              }
-            }).then(response => console.log(response));
-          });
-        }
-        else {
-          console.log("not logged in");
-        }
-      }
-    });
-    //http://stackoverflow.com/questions/32070227/reactjs-facebook-sdk-login-button-not-showing-up-the-second-time  
-    return (
-      <div>
-        <Button />
-      </div>
-    );
-  }
+
+    pingServer() {
+        this.props.fb.getLoginStatus((response) => {
+            if (response.status === 'connected') {
+                const accessToken = response.authResponse.accessToken;
+                if (response.authResponse) {
+                    console.log('Welcome!  Fetching your information.... ');
+                    this.props.fb.api('/me', { fields: 'name,email,id' }, function (response) {
+                        console.log('Good to see you, ' + response.name + '.');
+                        fetch('/api/login', {
+                            method: 'POST',
+                            body: JSON.stringify({ ...response, accessToken }),
+                            headers: {
+                                accept: 'application/json',
+                                "content-type": "application/json"
+                            }
+                        }).then(response => console.log(response));
+                    });
+                }
+                else {
+                    console.log("not logged in");
+                }
+            }
+        });
+    }
+    render() {
+
+        //http://stackoverflow.com/questions/32070227/reactjs-facebook-sdk-login-button-not-showing-up-the-second-time  
+        return (
+            <div>
+                <Button />
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,17 +6,17 @@ import App from './App';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 class About extends React.Component {
-  render() {
-    return (<h1>About page</h1>);
-  }
+    render() {
+        return (<h1>About page</h1>);
+    }
 }
 ReactDOM.render(
-  <Router>
-    <div>
-      <Link to="/about">About</Link>
-      <Route path='/about' component={About} />
-      <Route exact={true} path='/' render={() => <App fb={FB} />} />     
-    </div>
-  </Router>,
-  document.getElementById('root')
+    <Router>
+        <div>
+            <Link to="/about">About</Link>
+            <Route path='/about' component={About} />
+            <Route exact={true} path='/' render={() => <App fb={FB} />} />
+        </div>
+    </Router>,
+    document.getElementById('root')
 );

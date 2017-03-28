7a8f5c53bf5decc2aafb694ed4b4b3f39df61bbf
Ivan Mudrak <ivan.mudrak@ukr.net>
2017-03-28 22:18:20 +0300

    add try/catch to apl call

--- server/apl.js
+++ server/apl.js
@@ -1,14 +1,18 @@
-
 invokeApl = function (newAttemptDir) {
     return new Promise((resolve, reject) => {
-        var execFile = require('child_process').execFile;
-        const child =
-            execFile('C:\\Program Files\\Dyalog\\Dyalog APL-64 15.0 Unicode\\dyalog.exe',
-                ['C:\\Bond\\BondUnitJson.dws', '-STUDENT_PATH=' + `${newAttemptDir}`],
-                function (error, stdout, stderr) {
-                    console.log("Done APL call");
-                    resolve();
-                });
+        try {
+            var execFile = require('child_process').execFile;
+            const child =
+                execFile('C:\\Program Files\\Dyalog\\Dyalog APL-64 15.0 Unicode\\dyalog.exe', // UPDATED when MOVE
+                    ['C:\\Bond\\BondUnitJson.dws', '-STUDENT_PATH=' + `${newAttemptDir}`],    // UPDATED when MOVE
+                    function (error, stdout, stderr) {
+                        console.log("APL call succeed");
+                        resolve();
+                    });
+        }
+        catch (e) {
+            console.log("APL call failed");
+            reject();
+        }
     });
-
 }
